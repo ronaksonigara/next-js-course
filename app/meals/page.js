429eeedccbac54f634ae57c7a9ba387e7ed3b45f
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2024-04-20 22:53:23 +0530

    Adding Static Metadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,6 +5,11 @@ import { getMeals } from "@/lib/meals";
 import { Suspense } from "react";
 import MealsLoadingPage from "./loading-out";
 
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the delicious meals shared byour vibrant community.",
+};
+
 async function Meals() {
   const meals = await getMeals();
 
